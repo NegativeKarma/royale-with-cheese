48c839db2c1ba866f5578a465d3d15f16bb0b800
Francesco <francesco.falchi@gmail.com>
2017-12-20 19:39:07 +0100

    Add support for webmock and capybara

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,3 +15,15 @@ end
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
 World(FactoryBot::Syntax::Methods)
+
+Capybara.register_driver :chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: {
+          args: %w[ no-sandbox disable-popup-blocking disable-gpu window-size=1280,1024]
+      }
+  )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
+Capybara.javascript_driver = :chrome
